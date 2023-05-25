defcb4f0c0a02e074f68c6bf24bdc5922a050b4b
manishkarki358 <rajkarki358358@gmail.com>
2023-05-25 12:45:29 +0545

    desktop version completed

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ const content=document.querySelector('.content')
 const yes=document.querySelector('.yes')
 const no=document.querySelector('.no')
 const toshowifno=document.querySelector('.toshowifno')
+let bgmusic = new Audio('Loyalty_Freak_Music_-_07_-_A_really_dark_alley.mp3');
 yes.addEventListener('click',()=>{
-    let bgmusic = new Audio('Loyalty_Freak_Music_-_07_-_A_really_dark_alley.mp3');
   bgmusic.play()
     starting.style.display="none"
     content.style.display="block"
@@ -49,13 +49,154 @@ toshowifno.style.display="block"
     
     giffy.addEventListener('mouseover',()=>{
         jumpscare.play()
-        console.log("hovered");
     })
     giffy.addEventListener('mouseleave',()=>{
         jumpscare.pause()
     })
     //alert("enter esc to stop background music")
     
+ let screaming=new Audio('firstscream.mp3')   
+ let womanScreaming=new Audio('secondscream.wav')   
+ let door=new Audio('door.wav')   
+ let doom=new Audio('doom.mp3')  
+ let warning=new Audio('warning.wav') 
+    
+
+ function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function playMusicWhenInView() {
+  var triggerElement = document.querySelector('.story2');
+
+  if (isElementInViewport(triggerElement)) {
+    screaming.play();
+    bgmusic.volume=0.5
+
+  } else {
+    screaming.pause();
+    bgmusic.volume=1
+  }
+}
+
+window.addEventListener('scroll', playMusicWhenInView);
+
+
+function isElementInViewport2(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function playMusicWhenInView2() {
+  var triggerElement = document.querySelector('.story4');
+
+  if (isElementInViewport2(triggerElement)) {
+    doom.play();
+    bgmusic.volume=0.5
+
+  } else {
+    doom.pause();
+    bgmusic.volume=1
+  }
+}
+
+window.addEventListener('scroll', playMusicWhenInView2);
+
+
+function isElementInViewport3(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function playMusicWhenInView3() {
+  var triggerElement = document.querySelector('.story5');
+
+  if (isElementInViewport3(triggerElement)) {
+    door.play();
+    bgmusic.volume=0.7
+
+  } else {
+    door.pause();
+    bgmusic.volume=1
+  }
+}
+
+window.addEventListener('scroll', playMusicWhenInView3);
+
+
+
+
+
+function isElementInViewport4(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function playMusicWhenInView4() {
+  var triggerElement = document.querySelector('.story7');
+
+  if (isElementInViewport4(triggerElement)) {
+    womanScreaming.play();
+    womanScreaming.volume=0.5
+    bgmusic.volume=0.5
+
+  } else {
+    womanScreaming.pause();
+    bgmusic.volume=1
+  }
+}
+
+window.addEventListener('scroll', playMusicWhenInView4);
+
+
+function isElementInViewport5(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function playMusicWhenInView5() {
+  var triggerElement = document.querySelector('.story8');
+
+  if (isElementInViewport5(triggerElement)) {
+    bgmusic.volume=0.5
+    warning.play().then(()=>{
+      warning.play().then(()=>{
+        warning.play()
+      })
+    })
+    
     
     
 
+  } else {
+    bgmusic.volume=1
+  }
+}
+
+window.addEventListener('scroll', playMusicWhenInView5);
